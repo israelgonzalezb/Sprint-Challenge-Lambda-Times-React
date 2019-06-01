5c015cc3a100af5291e9a00b2ac4785d10a55348
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-05-31 21:01:43 -0700

    Added PropTypes checks

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Card = props => {
   return (
@@ -17,5 +18,9 @@ const Card = props => {
 };
 
 // Make sure to include PropTypes.
+
+Card.PropTypes = {
+  card: PropTypes.object
+};
 
 export default Card;
